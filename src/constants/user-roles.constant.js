45f76882e7c8f90a73f0041378b8d48f53fd4676
Tue Vo <vothaiminhtue0312@gmail.com>
2021-01-14 04:27:48 +0700

    declared user roles as constants

---
+++ src/constants/user-roles.constant.js
@@ -0,0 +1,18 @@
+export const userRoles = {
+  GUEST: {
+    value: 1,
+    name: 'Nặc danh'
+  },
+  STUDENT: {
+    value: 2,
+    name: 'Học viên'
+  },
+  LECTURER: {
+    value: 3,
+    name: 'Giảng viên'
+  },
+  ADMIN: {
+    value: 4,
+    name: 'Quản trị viên'
+  }
+}
