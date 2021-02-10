e6d1251bb231446d994549f8207e1f785a003ce2
Viettran0408 <viettran040898.vt@gmail.com>
2021-02-10 11:03:39 +0700

    get lecturer list

---
+++ src/api/lecturers.api.js
@@ -0,0 +1,15 @@
+import axiosClient from './axios-client';
+
+const baseUrl = '/lecturers';
+
+const lecturerApi = {
+  getAll: (page, limit) => {
+    let url = `${baseUrl}`;
+
+    if (page && limit) url += `?page=${page}&limit=${limit}`;
+
+    return axiosClient.get(url);
+  },
+};
+
+export default lecturerApi;
--- src/views/Users/Users.jsx
+++ src/views/Users/Users.jsx
@@ -1,4 +1,14 @@
-import { AppBar, Box, Button, Fab, GridList, GridListTile, Tab, Tabs, Tooltip } from '@material-ui/core';
+import {
+  AppBar,
+  Box,
+  Button,
+  Fab,
+  GridList,
+  GridListTile,
+  Tab,
+  Tabs,
+  Tooltip,
+} from '@material-ui/core';
 import FaceIcon from '@material-ui/icons/Face';
 import PersonIcon from '@material-ui/icons/Person';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
@@ -12,6 +22,7 @@ import { showNotification } from 'redux/actions/app.action';
 import { studentApi } from 'api';
 import NumberFormat from 'react-number-format';
 import AddLecturer from './components/AddLecturer/AddLecturer';
+import lecturerApi from 'api/lecturers.api';
 
 function a11yProps(index) {
   return {
@@ -20,45 +31,54 @@ function a11yProps(index) {
   };
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: '35rem',
     width: '100%',
     overflow: 'hidden',
-    borderRadius: theme.palette.card.borderRadius
+    borderRadius: theme.palette.card.borderRadius,
   },
   tabs: {
     boxShadow: 'none',
-    "backgroundColor": "#a4508b",
-    "backgroundImage": "linear-gradient(326deg, #a4508b 0%, #5f0a87 74%)"
+    backgroundColor: '#a4508b',
+    backgroundImage: 'linear-gradient(326deg, #a4508b 0%, #5f0a87 74%)',
   },
   btnAddLecturerContainer: {
     position: 'absolute',
     zIndex: 10,
     right: '5%',
-    top: '-5%'
+    top: '-5%',
   },
   btnAddLecturer: {
-    "backgroundColor": "#a4508b",
-    "backgroundImage": "linear-gradient(326deg, #a4508b 0%, #5f0a87 74%)"
+    backgroundColor: '#a4508b',
+    backgroundImage: 'linear-gradient(326deg, #a4508b 0%, #5f0a87 74%)',
   },
   btnLoadMore: {
-    "backgroundColor": "#a4508b",
-    "backgroundImage": "linear-gradient(326deg, #a4508b 0%, #5f0a87 74%)"
-  }
+    backgroundColor: '#a4508b',
+    backgroundImage: 'linear-gradient(326deg, #a4508b 0%, #5f0a87 74%)',
+  },
 }));
 
 const Users = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
-  const limit = 1;
+  const limit = 12;
 
   const [tabValue, setTabValue] = useState(1);
 
   const [studentList, setStudentList] = useState([]);
   const [studentListPage, setStudentListPage] = useState(1);
   const [studentListTotalItems, setStudentListTotalItems] = useState(0);
-  const [disableBtnLoadMoreStudent, setDisableBtnLoadMoreStudent] = useState(false);
+  const [disableBtnLoadMoreStudent, setDisableBtnLoadMoreStudent] = useState(
+    false
+  );
+
+  const [lecturerList, setLecturerList] = useState([]);
+  const [lecturerListPage, setLecturerListPage] = useState(1);
+  const [lecturerListTotalItems, setLecturerListTotalItems] = useState(0);
+  const [disableBtnLoadMoreLecturer, setDisableBtnLoadMoreLecturer] = useState(
+    false
+  );
 
   const [openAddLecturer, setOpenAddLecturer] = useState(false);
 
@@ -78,16 +98,41 @@ const Users = () => {
         if (newStudentList.length < totalItems) {
           setDisableBtnLoadMoreStudent(false);
         }
-
       } catch (error) {
         if (error.messages && error.messages.length > 0) {
           dispatch(showNotification('error', apiMessage[error.messages[0]]));
         }
       }
-    }
+    };
 
     getAllStudents();
   }, [studentListPage]);
+
+  useEffect(() => {
+    const getAllLecturers = async () => {
+      setDisableBtnLoadMoreLecturer(true);
+      try {
+        const res = await lecturerApi.getAll(lecturerListPage, limit);
+        const students = res.data.entries;
+
+        const { totalItems } = res.data.meta;
+        setLecturerListTotalItems(totalItems);
+
+        const newLecturerList = lecturerList.concat(students);
+        setLecturerList(newLecturerList);
+
+        if (newLecturerList.length < totalItems) {
+          setDisableBtnLoadMoreLecturer(false);
+        }
+      } catch (error) {
+        if (error.messages && error.messages.length > 0) {
+          dispatch(showNotification('error', apiMessage[error.messages[0]]));
+        }
+      }
+    };
+
+    getAllLecturers();
+  }, [lecturerListPage]);
 
   const students = [
     {
@@ -95,86 +140,86 @@ const Users = () => {
       fullName: 'Lana',
       avatarUrl: 'images/avatars/avatar_6.png',
       email: 'abc@gmail.com',
-      createdAt: new Date()
+      createdAt: new Date(),
     },
     {
       _id: 2,
       fullName: 'Lee Wei Shuan',
       avatarUrl: 'images/avatars/avatar_5.png',
       email: 'abc@gmail.com',
-      createdAt: new Date()
+      createdAt: new Date(),
     },
     {
       _id: 3,
       fullName: 'Steve Jonathan',
       avatarUrl: 'images/avatars/avatar_4.png',
       email: 'abc@gmail.com',
-      createdAt: new Date()
+      createdAt: new Date(),
     },
     {
       _id: 4,
       fullName: 'Steve Jonathan',
       avatarUrl: 'images/avatars/avatar_4.png',
       email: 'abc@gmail.com',
-      createdAt: new Date()
+      createdAt: new Date(),
     },
     {
       _id: 5,
       fullName: 'Steve Jonathan',
       avatarUrl: 'images/avatars/avatar_4.png',
       email: 'abc@gmail.com',
-      createdAt: new Date()
+      createdAt: new Date(),
     },
     {
       _id: 6,
       fullName: 'Lana',
       avatarUrl: 'images/avatars/avatar_6.png',
       email: 'abc@gmail.com',
-      createdAt: new Date()
+      createdAt: new Date(),
     },
     {
       _id: 7,
       fullName: 'Lee Wei Shuan',
       avatarUrl: 'images/avatars/avatar_5.png',
       email: 'abc@gmail.com',
-      createdAt: new Date()
+      createdAt: new Date(),
     },
     {
       _id: 8,
       fullName: 'Steve Jonathan',
       avatarUrl: 'images/avatars/avatar_4.png',
       email: 'abc@gmail.com',
-      createdAt: new Date()
+      createdAt: new Date(),
     },
     {
       _id: 9,
       fullName: 'Steve Jonathan',
       avatarUrl: 'images/avatars/avatar_4.png',
       email: 'abc@gmail.com',
-      createdAt: new Date()
+      createdAt: new Date(),
     },
     {
       _id: 10,
       fullName: 'Steve Jonathan',
       avatarUrl: 'images/avatars/avatar_4.png',
       email: 'abc@gmail.com',
-      createdAt: new Date()
+      createdAt: new Date(),
     },
     {
       _id: 11,
       fullName: 'Lana',
       avatarUrl: 'images/avatars/avatar_6.png',
       email: 'abc@gmail.com',
-      createdAt: new Date()
+      createdAt: new Date(),
     },
     {
       _id: 12,
       fullName: 'Lee Wei Shuan',
       avatarUrl: 'images/avatars/avatar_5.png',
       email: 'abc@gmail.com',
-      createdAt: new Date()
+      createdAt: new Date(),
     },
-  ]
+  ];
 
   const lecturers = [
     {
@@ -184,9 +229,12 @@ const Users = () => {
       email: 'abc@gmail.com',
       createdAt: new Date(),
       averageRating: 4.5,
-      numberOfCourses: 10,
-      numberOfStudents: 2500,
-      numberOfRatings: 2500
+      roleInfo: {
+        averageRating: 4.5,
+        numberOfCourses: 10,
+        numberOfStudents: 2500,
+        numberOfRatings: 2500,
+      },
     },
     {
       _id: 2,
@@ -194,10 +242,12 @@ const Users = () => {
       avatarUrl: 'images/avatars/avatar_5.png',
       email: 'abc@gmail.com',
       createdAt: new Date(),
-      averageRating: 4.5,
-      numberOfCourses: 10,
-      numberOfStudents: 2500,
-      numberOfRatings: 2500
+      roleInfo: {
+        averageRating: 4.5,
+        numberOfCourses: 10,
+        numberOfStudents: 2500,
+        numberOfRatings: 2500,
+      },
     },
     {
       _id: 3,
@@ -205,10 +255,12 @@ const Users = () => {
       avatarUrl: 'images/avatars/avatar_4.png',
       email: 'abc@gmail.com',
       createdAt: new Date(),
-      averageRating: 4.5,
-      numberOfCourses: 10,
-      numberOfStudents: 2500,
-      numberOfRatings: 2500
+      roleInfo: {
+        averageRating: 4.5,
+        numberOfCourses: 10,
+        numberOfStudents: 2500,
+        numberOfRatings: 2500,
+      },
     },
     {
       _id: 4,
@@ -216,10 +268,12 @@ const Users = () => {
       avatarUrl: 'images/avatars/avatar_4.png',
       email: 'abc@gmail.com',
       createdAt: new Date(),
-      averageRating: 4.5,
-      numberOfCourses: 10,
-      numberOfStudents: 2500,
-      numberOfRatings: 2500
+      roleInfo: {
+        averageRating: 4.5,
+        numberOfCourses: 10,
+        numberOfStudents: 2500,
+        numberOfRatings: 2500,
+      },
     },
     {
       _id: 5,
@@ -227,10 +281,12 @@ const Users = () => {
       avatarUrl: 'images/avatars/avatar_4.png',
       email: 'abc@gmail.com',
       createdAt: new Date(),
-      averageRating: 4.5,
-      numberOfCourses: 10,
-      numberOfStudents: 2500,
-      numberOfRatings: 2500
+      roleInfo: {
+        averageRating: 4.5,
+        numberOfCourses: 10,
+        numberOfStudents: 2500,
+        numberOfRatings: 2500,
+      },
     },
     {
       _id: 6,
@@ -238,10 +294,12 @@ const Users = () => {
       avatarUrl: 'images/avatars/avatar_6.png',
       email: 'abc@gmail.com',
       createdAt: new Date(),
-      averageRating: 4.5,
-      numberOfCourses: 10,
-      numberOfStudents: 2500,
-      numberOfRatings: 2500
+      roleInfo: {
+        averageRating: 4.5,
+        numberOfCourses: 10,
+        numberOfStudents: 2500,
+        numberOfRatings: 2500,
+      },
     },
     {
       _id: 7,
@@ -249,10 +307,12 @@ const Users = () => {
       avatarUrl: 'images/avatars/avatar_5.png',
       email: 'abc@gmail.com',
       createdAt: new Date(),
-      averageRating: 4.5,
-      numberOfCourses: 10,
-      numberOfStudents: 2500,
-      numberOfRatings: 2500
+      roleInfo: {
+        averageRating: 4.5,
+        numberOfCourses: 10,
+        numberOfStudents: 2500,
+        numberOfRatings: 2500,
+      },
     },
     {
       _id: 8,
@@ -260,10 +320,12 @@ const Users = () => {
       avatarUrl: 'images/avatars/avatar_4.png',
       email: 'abc@gmail.com',
       createdAt: new Date(),
-      averageRating: 4.5,
-      numberOfCourses: 10,
-      numberOfStudents: 2500,
-      numberOfRatings: 2500
+      roleInfo: {
+        averageRating: 4.5,
+        numberOfCourses: 10,
+        numberOfStudents: 2500,
+        numberOfRatings: 2500,
+      },
     },
     {
       _id: 9,
@@ -271,10 +333,12 @@ const Users = () => {
       avatarUrl: 'images/avatars/avatar_4.png',
       email: 'abc@gmail.com',
       createdAt: new Date(),
-      averageRating: 4.5,
-      numberOfCourses: 10,
-      numberOfStudents: 2500,
-      numberOfRatings: 2500
+      roleInfo: {
+        averageRating: 4.5,
+        numberOfCourses: 10,
+        numberOfStudents: 2500,
+        numberOfRatings: 2500,
+      },
     },
     {
       _id: 10,
@@ -282,10 +346,12 @@ const Users = () => {
       avatarUrl: 'images/avatars/avatar_4.png',
       email: 'abc@gmail.com',
       createdAt: new Date(),
-      averageRating: 4.5,
-      numberOfCourses: 10,
-      numberOfStudents: 2500,
-      numberOfRatings: 2500
+      roleInfo: {
+        averageRating: 4.5,
+        numberOfCourses: 10,
+        numberOfStudents: 2500,
+        numberOfRatings: 2500,
+      },
     },
     {
       _id: 11,
@@ -293,10 +359,12 @@ const Users = () => {
       avatarUrl: 'images/avatars/avatar_6.png',
       email: 'abc@gmail.com',
       createdAt: new Date(),
-      averageRating: 4.5,
-      numberOfCourses: 10,
-      numberOfStudents: 2500,
-      numberOfRatings: 2500
+      roleInfo: {
+        averageRating: 4.5,
+        numberOfCourses: 10,
+        numberOfStudents: 2500,
+        numberOfRatings: 2500,
+      },
     },
     {
       _id: 12,
@@ -304,12 +372,14 @@ const Users = () => {
       avatarUrl: 'images/avatars/avatar_5.png',
       email: 'abc@gmail.com',
       createdAt: new Date(),
-      averageRating: 4.5,
-      numberOfCourses: 10,
-      numberOfStudents: 2500,
-      numberOfRatings: 2500
+      roleInfo: {
+        averageRating: 4.5,
+        numberOfCourses: 10,
+        numberOfStudents: 2500,
+        numberOfRatings: 2500,
+      },
     },
-  ]
+  ];
 
   const handleTabChange = (event, newValue) => {
     setTabValue(newValue);
@@ -321,15 +391,19 @@ const Users = () => {
         const newPage = studentListPage + 1;
         setStudentListPage(newPage);
         break;
+      case 2:
+        const newLecturerPage = lecturerListPage + 1;
+        setLecturerListPage(newLecturerPage);
+        break;
 
       default:
         break;
     }
-  }
+  };
 
   const handleClickBtnAddLecturer = () => {
     setOpenAddLecturer(true);
-  }
+  };
 
   const handleCloseAddLecturer = (accepted, data) => {
     setOpenAddLecturer(false);
@@ -337,17 +411,28 @@ const Users = () => {
     if (accepted) {
       console.log(data);
     }
-  }
+  };
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" className={classes.tabs} color="primary">
-        <Tabs value={tabValue} onChange={handleTabChange} aria-label="simple tabs example">
+      <AppBar position='static' className={classes.tabs} color='primary'>
+        <Tabs
+          value={tabValue}
+          onChange={handleTabChange}
+          aria-label='simple tabs example'
+        >
           <Tab
             icon={<PersonIcon />}
             label={
               <span>
-                Học viên (<NumberFormat value={studentListTotalItems} displayType={'text'} thousandSeparator={'.'} decimalSeparator={','} />)
+                Học viên (
+                <NumberFormat
+                  value={studentListTotalItems}
+                  displayType={'text'}
+                  thousandSeparator={'.'}
+                  decimalSeparator={','}
+                />
+                )
               </span>
             }
             {...a11yProps(0)}
@@ -356,7 +441,14 @@ const Users = () => {
             icon={<FaceIcon />}
             label={
               <span>
-                Giảng viên (<NumberFormat value={lecturers.length} displayType={'text'} thousandSeparator={'.'} decimalSeparator={','} />)
+                Giảng viên (
+                <NumberFormat
+                  value={lecturerListTotalItems}
+                  displayType={'text'}
+                  thousandSeparator={'.'}
+                  decimalSeparator={','}
+                />
+                )
               </span>
             }
             {...a11yProps(1)}
@@ -366,10 +458,14 @@ const Users = () => {
 
       {tabValue === 0 && (
         <Box p={4}>
-          <GridList cellHeight="auto" cols={3}>
+          <GridList cellHeight='auto' cols={3}>
             {studentList.map((s, i) => (
               <GridListTile key={s._id}>
-                <Box m={1} className="animate__animated animate__fadeIn" style={{ animationDelay: `${0.1 * i}s` }}>
+                <Box
+                  m={1}
+                  className='animate__animated animate__fadeIn'
+                  style={{ animationDelay: `${0.1 * i}s` }}
+                >
                   <Student data={s} />
                 </Box>
               </GridListTile>
@@ -379,9 +475,9 @@ const Users = () => {
             <Button
               fullWidth
               className={classes.btnLoadMore}
-              variant="contained"
-              color="primary"
-              size="large"
+              variant='contained'
+              color='primary'
+              size='large'
               onClick={() => handleClickBtnLoadMore(1)}
               disabled={disableBtnLoadMoreStudent}
             >
@@ -392,19 +488,34 @@ const Users = () => {
       )}
 
       {tabValue === 1 && (
-        <Box p={4} position="relative">
-          <AddLecturer open={openAddLecturer} onClose={handleCloseAddLecturer} />
-          <Box className={`${classes.btnAddLecturerContainer} animate__animated animate__bounceIn`}>
-            <Tooltip title="Thêm giảng viên mới">
-              <Fab size="large" color="primary" aria-label="add" className={classes.btnAddLecturer} onClick={handleClickBtnAddLecturer}>
-                <PersonAddIcon fontSize="large" />
+        <Box p={4} position='relative'>
+          <AddLecturer
+            open={openAddLecturer}
+            onClose={handleCloseAddLecturer}
+          />
+          <Box
+            className={`${classes.btnAddLecturerContainer} animate__animated animate__bounceIn`}
+          >
+            <Tooltip title='Thêm giảng viên mới'>
+              <Fab
+                size='large'
+                color='primary'
+                aria-label='add'
+                className={classes.btnAddLecturer}
+                onClick={handleClickBtnAddLecturer}
+              >
+                <PersonAddIcon fontSize='large' />
               </Fab>
             </Tooltip>
           </Box>
-          <GridList cellHeight="auto" cols={3}>
-            {lecturers.map((s, i) => (
+          <GridList cellHeight='auto' cols={3}>
+            {lecturerList.map((s, i) => (
               <GridListTile key={s._id}>
-                <Box m={1} className="animate__animated animate__fadeIn" style={{ animationDelay: `${0.1 * i}s` }}>
+                <Box
+                  m={1}
+                  className='animate__animated animate__fadeIn'
+                  style={{ animationDelay: `${0.1 * i}s` }}
+                >
                   <Lecturer data={s} />
                 </Box>
               </GridListTile>
@@ -414,17 +525,17 @@ const Users = () => {
             <Button
               fullWidth
               className={classes.btnLoadMore}
-              variant="contained"
-              color="primary"
-              size="large"
+              variant='contained'
+              color='primary'
+              size='large'
               onClick={() => handleClickBtnLoadMore(2)}
+              disabled={disableBtnLoadMoreLecturer}
             >
               Xem thêm giảng viên
             </Button>
           </Box>
         </Box>
       )}
-
     </div>
   );
 };
